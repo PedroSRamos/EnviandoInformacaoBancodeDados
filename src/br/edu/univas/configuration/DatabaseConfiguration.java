bd6f696c377aed0e5c9cd933891032bb761d50a0
Pedro Henrique de Souza Ramos <36521649+PedroSRamos@users.noreply.github.com>
2021-11-30 21:42:28 -0300

    Update DatabaseConfiguration.java

--- src/br/edu/univas/configuration/DatabaseConfiguration.java
+++ src/br/edu/univas/configuration/DatabaseConfiguration.java
@@ -5,7 +5,6 @@ public class DatabaseConfiguration {
 	private static final String HOST = "192.168.1.1";
 	private static final String PORT = "4321";
 	private static final String DATABASE = "mydatabase";
-	
 	private static DatabaseConfiguration databaseInstance;
 	private DatabaseConfiguration() {}
 
